354ad8161321ca8b58921aaafb67f373204396f6
Seongho Bae <me@seonghobae.me>
2026-09-05 03:08:14 +0900

    test(zotero): clear owned rule revisions

--- crates/conceptweave-zotero/tests/steward_review_finalization.rs
+++ crates/conceptweave-zotero/tests/steward_review_finalization.rs
@@ -76,7 +76,7 @@ fn finalization_rejects_each_invalid_identity_coordinate() {
     let worksheet = complete_worksheet();
 
     let mut invalid_report = report();
-    invalid_report.rule_revision = "";
+    invalid_report.rule_revision.clear();
     assert_eq!(
         reviewed_golden_set_from_worksheet(&invalid_report, &worksheet, approval(&worksheet)),
         Err(EvaluationError::InvalidReview)
